46692f640a3b004fe5e378c7b113ed268890c3bd
Christian Johansson <christian@cvj.se>
2018-07-11 17:03:49 +0200

    Started with HTTP parsing

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::str;
 
 pub struct HttpRequest {
     get_arguments: HashMap<String, String>,
@@ -8,14 +9,30 @@ pub struct HttpRequest {
     request_uri: String
 };
 
+enum HttpRequest_Method {
+    CONNECT,
+    DELETE,
+    GET,
+    HEAD,
+    OPTIONS,
+    PATCH,
+    POST,
+    PUT,
+    TRACE
+}
+
 impl HttpRequest {
     // TODO Implement this
-    pub fn matches(&[u8]) -> bool {
+    // This function answers whether request is a HTTP request or not
+    pub fn matches(request: &[u8]) -> bool {
+        if let Some(request) = str::from_utf8(buffer) {
+            
+        }
         false
     }
 
     // TODO Implement this
-    pub fn from_tcp_stream(&[u8]) -> Result<HttpRequest, &'static str> {
+    pub fn from_tcp_stream(request: &[u8]) -> Result<HttpRequest, &'static str> {
         Err("Failed to parse stream")
     }
 }
@@ -23,6 +40,14 @@ impl HttpRequest {
 #[cfg(test)]
 mod request_test {
     #[test]
+    fn test_from_tcp_stream() {
+        
+    }
+
+    #[test]
+    fn test_matches() {
+        
+    }
 }
 
 
