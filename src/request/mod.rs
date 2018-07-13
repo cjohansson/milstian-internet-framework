bb976d80e3b87a190b18f8503322c98c26782d12
Christian Johansson <christian@cvj.se>
2018-07-13 12:07:36 +0200

    HTTP method URI and protocol is now parsed

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -10,22 +10,33 @@ pub struct HttpRequest {
     request_uri: String
 }
 
+#[derive(Debug)]
 enum HttpRequestMethod {
-    CONNECT,
-    DELETE,
-    GET,
-    HEAD,
-    OPTIONS,
-    PATCH,
-    POST,
-    PUT,
-    TRACE
+    Connect,
+    Delete,
+    Get,
+    Head,
+    Invalid,
+    Options,
+    Patch,
+    Post,
+    Put,
+    Trace
+}
+
+#[derive(Debug)]
+enum HttpRequestProtocol {
+    Invalid,
+    TwoDotZero,
+    OneDotZero,
+    OneDotOne,
+    ZeroDotNine,
 }
 
 enum HttpRequestSection {
-    REQUEST_LINE,
-    HEADER_FIELDS,
-    MESSAGE_BODY
+    RequestLine,
+    HeaderFields,
+    MessageBody
 }
 
 impl HttpRequest {
@@ -37,25 +48,89 @@ impl HttpRequest {
             println!("Request as string: {}", request);
             if request.is_ascii() {
                 println!("Request is ASCII");
-                let mut section = HttpRequestSection::REQUEST_LINE;
+                let mut section = HttpRequestSection::RequestLine;
+                let mut error = false;
                 for line in request.lines() {
                     match section {
-                        HttpRequestSection::REQUEST_LINE => {
-                            // TODO: Do stuff here
-                            section = HttpRequestSection::HEADER_FIELDS;
+                        HttpRequestSection::RequestLine => {
+                            let mut request_method: HttpRequestMethod = HttpRequestMethod::Invalid;
+                            let mut request_uri: String;
+                            let mut request_protocol: HttpRequestProtocol = HttpRequestProtocol::Invalid;
+
+                            let parts: Vec<&str> = line.split(" ").collect();
+                            println!("Collect request-line parts {:?}", &parts);
+                            if parts.len() > 1 {
+
+                                // Parse method
+                                if let Some(method_string) = parts.get(0) {
+                                    println!("method_string: {}", method_string);
+                                    request_method = match method_string.as_ref() {
+                                        "CONNECT" => HttpRequestMethod::Connect,
+                                        "DELETE" => HttpRequestMethod::Delete,
+                                        "GET" => HttpRequestMethod::Get,
+                                        "HEAD" => HttpRequestMethod::Head,
+                                        "OPTIONS" => HttpRequestMethod::Options,
+                                        "PATCH" => HttpRequestMethod::Patch,
+                                        "PUT" => HttpRequestMethod::Put,
+                                        "POST" => HttpRequestMethod::Post,
+                                        "TRACE" => HttpRequestMethod::Trace,
+                                        __ => {
+                                            error = true;
+                                            HttpRequestMethod::Invalid
+                                        }
+                                    };
+                                } else {
+                                    error = true;
+                                }
+
+                                // Parse request URI
+                                if let Some(request_uri) = parts.get(1) {
+                                    println!("Found request uri: {}", &request_uri);
+                                } else {
+                                    error = true;
+                                }
+
+                                // Parse protocol here
+                                if let Some(request_protocol_string) = parts.get(2) {
+                                    request_protocol = match request_protocol_string.as_ref() {
+                                        "HTTP/0.9" => HttpRequestProtocol::ZeroDotNine,
+                                        "HTTP/1.0" => HttpRequestProtocol::OneDotZero,
+                                        "HTTP/1.1" => HttpRequestProtocol::OneDotOne,
+                                        "HTTP/2.0" => HttpRequestProtocol::TwoDotZero,
+                                        _ => {
+                                            error = true;
+                                            HttpRequestProtocol::Invalid
+                                        }
+                                    };
+                                    println!("Found protocol: {:?}", &request_protocol);
+                                } else {
+                                    error = true;
+                                }
+
+                                if !error {
+                                    println!("No error, proceeding to header fields");
+                                    section = HttpRequestSection::HeaderFields;
+                                }
+
+                            } else {
+                                error = true;
+                            }
                         },
-                        HttpRequestSection::HEADER_FIELDS => {
+                        HttpRequestSection::HeaderFields => {
                             if line.trim().is_empty() {
-                                section = HttpRequestSection::MESSAGE_BODY;
+                                section = HttpRequestSection::MessageBody;
                             } else {
                                 // TODO: Do stuff here
                             }
                         },
-                        HttpRequestSection::MESSAGE_BODY => {
+                        HttpRequestSection::MessageBody => {
                             if !line.is_empty() {
                                 // TODO: Do stuff here
                             }
                         }
+                    }
+                    if error {
+                        break;
                     }
                 }
             }
@@ -70,9 +145,30 @@ mod request_test {
     #[test]
     fn test_from_tcp_stream() {
         let response = HttpRequest::from_tcp_stream(
-            b"GET /\r\n"
+            b"GET / HTTP/0.9\r\n"
         );
         assert!(response.is_some());
+
+        let response = HttpRequest::from_tcp_stream(
+            b"GET / HTTP/1.0\r\n"
+        );
+        assert!(response.is_some());
+
+        let response = HttpRequest::from_tcp_stream(
+            b"GET / HTTP/1.1\r\n"
+        );
+        assert!(response.is_some());
+
+        let response = HttpRequest::from_tcp_stream(
+            b"GET / HTTP/2.0\r\n"
+        );
+        assert!(response.is_some());
+
+        let response = HttpRequest::from_tcp_stream(
+            b"GET / HTTP/2.2\r\n"
+        );
+        assert!(response.is_none());
+
     }
 }
 
